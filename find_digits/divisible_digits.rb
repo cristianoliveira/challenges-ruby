b386799e3801b7faa9f18bac7c884a7d69c9815a
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-02-11 23:37:59 -0200

    Find_Digits: rubish

--- find_digits/divisible_digits.rb
+++ find_digits/divisible_digits.rb
@@ -2,7 +2,7 @@ class DivisibleDigits
   def self.count_for(number)
     return 0 if number.nil? || number.zero?
 
-    digits = number.to_s.chars.map{ |d| d.to_i }
+    digits = number.to_s.chars.map(&:to_i)
     digits.map{ |d| !d.zero? && number % d == 0  }.count(true)
   end
 end
