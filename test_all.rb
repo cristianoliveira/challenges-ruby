116588d7239f7660d198f5973e273a174681fd44
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-06-18 20:15:48 +0200

    change rakefile

--- test_all.rb
+++
@@ -1,8 +0,0 @@
-require 'rake/testtask'
-
-    Rake::TestTask.new do |t|
-      t.libs = ["lib"]
-      t.warning = true
-      t.verbose = true
-      t.test_files = FileList['spec/*_spec.rb']
-    end
